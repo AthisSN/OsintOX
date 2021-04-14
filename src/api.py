22cd27a5a86f5c753dbde66fe9ad1538aa7108df
Nadar Athis <53950135+Nadar-Athis@users.noreply.github.com>
2021-04-14 15:43:19 +0530

    Update api.py

--- src/api.py
+++ src/api.py
@@ -1,1 +1,4 @@
+def macvendor():
+        macapi = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJtYWN2ZW5kb3JzIiwiZXhwIjoxODc1NzA1MjcxLCJpYXQiOjE1NjEyMDkyNzEsImlzcyI6Im1hY3ZlbmRvcnMiLCJqdGkiOiI1OGYwNDQyZi1kMjkyLTRlYjQtOTAwYy1jMWYxYWMzZjZlYjQiLCJuYmYiOjE1NjEyMDkyNzAsInN1YiI6IjE2MDMiLCJ0eXAiOiJhY2Nlc3MifQ.evlDM70N0GBK3qT4TF6cAZa7_en6ZaqlpxlarFYaoeTJBB1guNoCWcXjTpRBep80Wy60peEwGIwkx-7cXLqrSQ"
+        return macapi
 
